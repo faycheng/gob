3e2b545e31efbf65c613b9b170436ffc0a6e0dc5
程飞 <fay.cheng.cn@gmail.com>
2019-05-14 19:09:35 +0800

    add PluginClient

---
+++ plugin/server/client.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"context"
+	"github.com/faycheng/gob/plugin/server/api"
+	"google.golang.org/grpc"
+)
+
+type PluginClient struct {
+	path   string
+	conn   *grpc.ClientConn
+	client proto.TaskServiceClient
+}
+
+func NewPluginClient(path string) *PluginClient {
+	// TODO: parse the unix socket path
+	conn, err := grpc.Dial("/")
+	client := proto.NewTaskServiceClient(conn)
+	if err != nil {
+		panic(err)
+	}
+	return &PluginClient{
+		path:   path,
+		conn:   conn,
+		client: client,
+	}
+}
+
+func (c *PluginClient) Call(c context.Context, method, args string) (err error) {
+	req := &proto.CallReq{
+		Method: method,
+		Args:   args,
+	}
+	_, err = c.client.Call(c, req)
+	return
+}
