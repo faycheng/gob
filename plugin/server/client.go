7961754140ef431049d73787b25b4dc83b85f7a7
程飞 <fay.cheng.cn@gmail.com>
2019-05-15 19:46:20 +0800

    add PluginClient.Dial that can create a new task client to invoke grpc calls

--- plugin/server/client.go
+++ plugin/server/client.go
@@ -7,26 +7,33 @@ import (
 )
 
 type PluginClient struct {
-	path   string
-	conn   *grpc.ClientConn
+	addr   string
 	client proto.TaskServiceClient
 }
 
-func NewPluginClient(path string) *PluginClient {
+func NewPluginClient(addr string) *PluginClient {
 	// TODO: parse the unix socket path
-	conn, err := grpc.Dial("/")
-	client := proto.NewTaskServiceClient(conn)
-	if err != nil {
-		panic(err)
-	}
 	return &PluginClient{
-		path:   path,
-		conn:   conn,
-		client: client,
+		addr: addr,
 	}
 }
 
+func (c *PluginClient) Dial(ctx context.Context) (client proto.TaskServiceClient, err error) {
+	// TODO: lock
+	conn, err := grpc.Dial(c.addr, grpc.WithInsecure())
+	if err != nil {
+		return
+	}
+	return proto.NewTaskServiceClient(conn), nil
+}
+
 func (c *PluginClient) Call(ctx context.Context, method, args string) (err error) {
+	if c.client == nil {
+		c.client, err = c.Dial(ctx)
+		if err != nil {
+			return
+		}
+	}
 	req := &proto.CallReq{
 		Method: method,
 		Args:   args,
