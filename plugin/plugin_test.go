1574e6bde6962666b0ed65dac79aa80021ce61b9
程飞 <fay.cheng.cn@gmail.com>
2019-05-15 23:05:08 +0800

    add the benchmark for Task.Run

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/stretchr/testify/assert"
 	"testing"
-	"time"
 )
 
 func TestPlugin_Run(t *testing.T) {
@@ -18,8 +17,33 @@ func TestPlugin_Run(t *testing.T) {
 	assert.Empty(t, err)
 	tasks, err := plugin.Tasks()
 	assert.Empty(t, err)
-	// Wait the sub-process of plugin
-	time.Sleep(1 * time.Second)
 	err = tasks["echo"].Call(context.TODO(), `{"msg": "hello world"}`)
 	assert.Empty(t, err)
+}
+
+// TODO: optimize the performance of task.Run
+func BenchmarkPlugin_TaskRun(b *testing.B) {
+	config := &PluginConfig{
+		Name:  "echo",
+		Path:  "../example/plugin/echo",
+		Tasks: []string{"echo"},
+	}
+	plugin := NewPlugin(config)
+	err := plugin.Run()
+	if err != nil {
+		panic(err)
+	}
+	tasks, err := plugin.Tasks()
+	if err != nil {
+		panic(err)
+	}
+	echoTask := tasks["echo"]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err = echoTask.Call(context.TODO(), `{"msg": "hello world"}`)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
