80517d4f15fa6383a7486ac559b1bab675058f8c
程飞 <fay.cheng.cn@gmail.com>
2019-05-18 18:13:24 +0800

    refactor metric, add metric factory to create the counter or gauge

--- metric/metric.go
+++ metric/metric.go
@@ -1,19 +1,12 @@
 package metric
 
 import (
-	"github.com/faycheng/rolling"
 	"sync"
 	"time"
+
+	"github.com/faycheng/rolling"
+	"github.com/influxdata/influxdb/client/v2"
 )
-
-type counter struct {
-	tags    []tag
-	rolling *rolling.RollingCounter
-}
-
-func newCounter() *counter {
-	return &counter{}
-}
 
 type tag struct {
 	key   string
@@ -27,44 +20,73 @@ func WithTag(key string, value string) tag {
 	}
 }
 
-type Metric struct {
+type metricFactoryOpt func(*metricFactory)
+
+func WithInfluxDB(addr, username, password string) metricFactoryOpt {
+	influxClient, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     addr,
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return func(factory *metricFactory) {
+		factory.enableInfluxDB = true
+		factory.influxClient = influxClient
+	}
+
+}
+
+type metricFactory struct {
+	enableInfluxDB bool
+	influxClient   client.Client
+}
+
+func NewMetricFacotry(opts ...metricFactoryOpt) *metricFactory {
+	factory := &metricFactory{}
+	for _, opt := range opts {
+		opt(factory)
+	}
+	return factory
+}
+
+func (f *metricFactory) NewCounter(tags ...tag) (counter *metric, err error) {
+	timingCounter := rolling.NewTimingCounter(rolling.TimingCounterOpts{
+		Size:           3600,
+		BucketDuration: time.Second,
+	})
+	counter = &metric{
+		tags:   tags,
+		metric: timingCounter,
+	}
+
+	return
+}
+
+func (f *metricFactory) NewGauge(tags ...tag) (gauge *metric, err error) {
+	timingCounter := rolling.NewTimingGauge(rolling.TimingGaugeOpts{
+		Size:           3600,
+		BucketDuration: time.Second,
+	})
+	gauge = &metric{
+		tags:   tags,
+		metric: timingCounter,
+	}
+	if f.enableInfluxDB {
+		gauge.influxClient = f.influxClient
+	}
+	return
+
+}
+
+type metric struct {
 	sync.Mutex
-	tags    []tag
-	counter rolling.Metric
-	gauge   rolling.Metric
+	influxClient client.Client
+	tags         []tag
+	metric       rolling.Metric
 }
 
-func NewMetric(tags ...tag) *Metric {
-	counterOpts := rolling.RollingCounterOpts{
-		Size:           10,
-		BucketDuration: time.Millisecond * 100,
-	}
-	counter := rolling.NewRollingCounter(counterOpts)
-	gaugeOpts := rolling.RollingGaugeOpts{
-		Size:           10,
-		BucketDuration: time.Millisecond * 100,
-	}
-	gauge := rolling.NewRollingGauge(gaugeOpts)
-	return &Metric{
-		tags:    tags,
-		counter: counter,
-		gauge:   gauge,
-	}
-}
-
-func (m *Metric) Incr() {
-	m.Lock()
-	defer m.Unlock()
-	m.counter.Add(1)
-}
-
-func (m *Metric) Gauge(value int64) {
-	m.Lock()
-	defer m.Unlock()
-	m.gauge.Add(value)
-}
-
-func (m *Metric) Push() {
-	m.Lock()
-	defer m.Unlock()
+func (m *metric) Add(val int64) {
+	m.metric.Add(val)
 }
